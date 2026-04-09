3b63a0f78de234993192614004b6ce22d84ea1b1
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2026-04-09 18:33:49 +0800

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-black py-12 text-center text-xs text-gray-500 border-t border-zinc-900 mt-auto">
+      VidStream • Free Anime Streaming • For demonstration purposes only
+    </footer>
+  );
+}
